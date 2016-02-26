41cafcd0c96935760b84e92f97845afa83732468
Cory Wandling <cwandling@pillartechnology.com>
2016-02-25 22:19:28 -0600

    Add hsqldb

---
+++ src/test/resources/db/init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE member (
+    id integer,
+    first varchar(128),
+    middle varchar(128),
+    last varchar(128)
+)
+
+INSERT INTO members VALUES
+(1, 'Cory', 'L', 'Wandling'),
+(2, 'Rhonda', 'K', 'Wandling'),
+(3, 'Kyle', 'M', 'Wandling'),
+(4, 'Robert', 'M', 'Wandling'),
+(5, 'Cherie', 'F', 'Wandling'),
+(6, 'Stephanie', 'F', 'Wandling'),
+(7, 'Emily', 'B', 'Wandling'),
+(8, 'James', 'C', 'Wandling'),
+(9, 'Marjorie', 'B', 'Wandling'),
+(10, 'Nathaniel', 'L', 'Wandling'),
+(11, 'Tina', 'D', 'Wandling'),
+(12, 'Aaron', 'J', 'Wandling')
